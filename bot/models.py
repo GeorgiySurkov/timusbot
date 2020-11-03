352d3e343f676828f4eff003190edef8f9a0f7b7
Goshan <georgiy.surkov@gmail.com>
2020-11-04 00:34:42 +0500

    Models for group and tracked user

--- bot/models.py
+++ bot/models.py
@@ -1,2 +1,18 @@
 from tortoise.models import Model
 from tortoise import fields
+
+
+class Group(Model):
+    class Meta:
+        table = 'group'
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=128)
+    telegram_id = fields.IntField(unique=True, index=True)
+    tracked_users = fields.ManyToManyField('models.TimusUser', related_name='tracked_in')
+
+
+class TimusUser(Model):
+    class Meta:
+        table = 'timus_user'
+    id = fields.IntField(pk=True)
+    timus_id = fields.IntField()
