aa09a3535691d04b4efc215f99c3c5cd0697645f
Goshan <georgiy.surkov@gmail.com>
2020-11-05 19:11:12 +0500

    fix minor bugs

--- bot/services/message_formers.py
+++ bot/services/message_formers.py
@@ -10,19 +10,19 @@ async def form_leaderboard_message(group: GroupModel) -> str:
     text = "Рейтинг по количеству решенных задач:\n"
     for i, user in sorted(group.tracked_users):
         # TODO: write correct form of word using pymorphy2
-        text += f'{i}) {user.username} - {user.solved_problems_amount} задач'
+        text += f'{i}) {escape_md(user.username)} - {user.solved_problems_amount} задач'
     return text
 
 
 def form_submission_message(submission: Submission) -> str:
     text = f'Задача {submission.problem.id}\. *{escape_md(submission.problem.name)}*\n'
-    text += f'Отправил {submission.author.username}\n'
+    text += f'Отправил {escape_md(submission.author.username)}\n'
     text += f'Результат проверки:\n'
     if submission.verdict == Verdict.accepted:
         text += '✅ '
     else:
         text += '❌ '
-    text += f'{submission.verdict.value}\n'
+    text += f'{escape_md(submission.verdict.value)}\n'
     text += f'_Язык:_ {escape_md(submission.language)}\n'
     if submission.test_number is not None:
         text += f'_№ теста:_ {submission.test_number}\n'
