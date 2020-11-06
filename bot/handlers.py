6b008ca69c3bc90052dd825c8222c2dd4075bb95
GeorgiySurkov <georgiy.surkov@gmail.com>
2020-11-06 21:46:46 +0500

    more logging

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.utils import exceptions as ex
+from logging import getLogger
 
 from . import dp, bot
 from .models import GroupModel, TimusUserModel
@@ -8,6 +9,8 @@ from .services.parser.timus_user import TimusUser
 from .services.message_formers import form_leaderboard_message
 from .services.command_parser import parse_track_command, parse_untrack_command
 from .services import exceptions as exc
+
+logger = getLogger(__name__)
 
 
 @dp.message_handler(commands=['send_leaderboard'])
@@ -19,6 +22,7 @@ async def send_leaderboard(msg: types.Message) -> None:
         except ex.MessageError:
             pass
     answer = await msg.answer(await form_leaderboard_message(group))
+    logger.info(f'Sent leaderboard to group with id={msg.chat.id}, title={msg.chat.title}')
     group.leaderboard_message_id = answer.message_id
     await group.save()
 
@@ -55,6 +59,7 @@ async def track(msg: types.Message) -> None:
                          f'Отвяжите какого-нибудь пользователя, чтобы добавить нового')
         return
     await group.tracked_users.add(timus_user_model)
+    logger.info(f'Started tracking user "{timus_user.username}" in group with id={group.telegram_id}')
     await msg.answer(f'Добавил {timus_user.username} к списку отслеживаемых пользователей.')
 
 
@@ -84,6 +89,7 @@ async def untrack(msg: types.Message) -> None:
     await timus_user_model.fetch_related('tracked_in')
     if len(timus_user_model.tracked_in) == 0 and not is_created:
         await timus_user_model.delete()
+        logger.info(f'Stopped tracking user "{timus_user.username}" in group with id={group.telegram_id}')
 
 
 @dp.message_handler(commands=['search'])
@@ -108,6 +114,7 @@ async def search(msg: types.Message) -> None:
         if len(result_text) + len(user_s) > 4096:
             break
         result_text += user_s
+    logger.info(f'Searched for users in group id={msg.chat.id}, title="{msg.chat.title}"')
     await msg.answer(result_text)
 
 
@@ -127,6 +134,7 @@ async def added_to_group(msg: types.Message) -> None:
                      'Я могу вести рейтинг и отслеживать посылки привязанных аккаунтов\.\n'
                      'Чтобы привязать аккаунт напиши _/search \<username\>_\n'
                      'Например _/search georgiysurkov_', parse_mode=types.ParseMode.MARKDOWN_V2)
+    logger.info(f'Added to group with id={msg.chat.id}, title="{msg.chat.title}"')
 
 
 @dp.message_handler()
