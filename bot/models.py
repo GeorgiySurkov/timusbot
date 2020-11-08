6f0756dfffb3c42d9d7f62cfe74b05fdaaa6c651
GeorgiySurkov <georgiy.surkov@gmail.com>
2020-11-08 15:28:51 +0500

    use BigIntField for telegram id in db

--- bot/models.py
+++ bot/models.py
@@ -6,7 +6,7 @@ class GroupModel(Model):
     class Meta:
         table = 'group'
     id = fields.IntField(pk=True)
-    telegram_id = fields.IntField(unique=True, index=True)
+    telegram_id = fields.BigIntField(unique=True, index=True)
     tracked_users = fields.ManyToManyField('models.TimusUserModel', related_name='tracked_in')
     leaderboard_message_id = fields.IntField(null=True, default=None)
 
@@ -15,7 +15,7 @@ class TimusUserModel(Model):
     class Meta:
         table = 'timus_user'
     id = fields.IntField(pk=True)
-    timus_id = fields.IntField(index=True)
+    timus_id = fields.BigIntField(index=True)
     username = fields.CharField(128, default='')
     solved_problems_amount = fields.IntField(null=True, default=None)
     joined = fields.DatetimeField(auto_now_add=True)
